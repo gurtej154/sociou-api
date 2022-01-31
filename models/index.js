8e16922008ded518160cab6d4b178c8241b8c6fb
Gurtej736l <gurtej154@gmail.com>
2022-01-31 11:30:50 +1030

    update repository

---
+++ models/index.js
@@ -0,0 +1,6 @@
+// Require users and thoughts models
+const User = require("./User");
+const Thought = require("./Thought");
+
+// Export modules: User and Thoughts
+module.exports = { User, Thought };
---
+++ server.js
@@ -0,0 +1,20 @@
+// Require express and mongoose
+const express = require("express");
+const mongoose = require("mongoose");
+const db = require("./config/connection");
+const routes = require("./routes");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.use(require("./routes"));
+
+// Log mongoose queries
+db.once("open", () => {
+  app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
+});
