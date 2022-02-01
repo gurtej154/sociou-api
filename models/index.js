da71d45515c9cc25abc753a62bc3e1461f55aeac
Gurtej736l <gurtej154@gmail.com>
2022-02-01 16:40:39 +1030

    update index.js, thoughtsController and userController js files

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 // Require users and thoughts models
-const User = require("./User");
-const Thought = require("./Thought");
+const User = require("./Users");
+const Thought = require("./Thoughts");
 
 // Export modules: User and Thoughts
 module.exports = { User, Thought };
